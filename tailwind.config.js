325533c182cad7cd6cfa99839932797976284596
faiz.j <faiz.j@softwareseni.com>
2021-07-11 19:35:24 +0700

    feat: Added a markup, styling and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,52 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      '0': '0',
+      '1/4': '25vh',
+      '1/2': '50vh',
+      '3/4': '75vh',
+      'full': '100vh',
+     },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      red: colors.red,
+      blueGray:colors.blueGray,
+      blue: {
+        lighter: '#8A96EE',
+        light:'#263BAB',
+        DEFAULT:'#13207B',
+        dark: '#233B6E',
+      },
+      gray: {
+        darkest: '#1f2d3d',
+        dark: '#3c4858',
+        DEFAULT: '#c0ccda',
+        light: '#e0e6ed',
+        lightest: '#f9fafc',
+      },
+      white: {
+        DEFAULT:'#fff'
+      }
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+      display:['group-hover']
+    },
   },
   plugins: [],
 }
