95e5012fcba1e7d882269b25f2a7c7c7856c2453
faiz.j <faiz.j@softwareseni.com>
2021-07-11 19:54:45 +0700

    feat: Added a tailwind configuration and other adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-    './src/**/*.html',
-    './src/**/*.vue',
-    './src/**/*.jsx',
+    './index.html',
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
